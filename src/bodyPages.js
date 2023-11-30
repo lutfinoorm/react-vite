ed65a5329fa943bc5bb0448b1d3f8f6fbae23c4d
lutfinoor muhamad <lutfinoor.muhamad@gmail.com>
2023-11-30 11:28:51 +0700

    feat: adding project folder

---
+++ src/bodyPages.js
@@ -0,0 +1,14 @@
+document.body.style.margin = '0'
+
+const navBar = document.getElementById('nav-bar');
+navBar.style.backgroundColor = 'grey';
+navBar.style.margin = '0';
+
+
+const paragraphEl = document.createElement('a');
+paragraphEl.innerText = "Homepage";
+paragraphEl.href = '../../index.html'
+paragraphEl.style.color = 'whitesmoke';
+paragraphEl.style.textDecoration = 'none';
+
+navBar.append(paragraphEl);
